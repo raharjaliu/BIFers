f6b814d2837d51e8d350971ca8bd9c2e56bbe5bd
Julian Schmidt <jschmidt@i12k-biolab06.informatik.tu-muenchen.de>
2016-12-04 20:57:39 +0100

    fix generate classes

--- scripts/generate_set_labels.py
+++ scripts/generate_set_labels.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import math
 import os.path
@@ -5,19 +7,18 @@ import re
 
 pos_list_file = sys.argv[1] + ".pos.list"
 neg_list_file = sys.argv[1] + ".neg.list"
-classes_files = sys.argv[1] + ".labels"
+classes_files = sys.argv[1] + ".classes"
 
-pos_list =  open(pos_list_file, 'r').read().split('\n'):
-neg_list =  open(neg_list_file, 'r').read().split('\n'):
+pos_list =  open(pos_list_file, 'r').read().split('\n')
+neg_list =  open(neg_list_file, 'r').read().split('\n')
 
-
-classes_out = open(classes_files, 'w');
+classes_out = open(classes_files, 'w')
 for prot in pos_list:
-	acc = prot[0].split("|")[1]
+	acc = prot.split('\t')[0]
 	classes_out.write('>' + acc + '\npositive\n')
 
 for prot in neg_list:
-	acc = prot[0].split("|")[1]
+	acc = prot.split('|')[1]
 	classes_out.write('>' + acc + '\nnegatvie\n')
 
 classes_out.close()
