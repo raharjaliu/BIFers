8a51fa13d4722c3fb1b2bd2c0bbd3db82b6649d7
Julian Schmidt <seoman@web.de>
2016-12-04 19:47:16 +0100

    generate set labels

---
+++ scripts/generate_set_labels.py
@@ -0,0 +1,23 @@
+import sys
+import math
+import os.path
+import re
+
+pos_list_file = sys.argv[1] + ".pos.list"
+neg_list_file = sys.argv[1] + ".neg.list"
+classes_files = sys.argv[1] + ".labels"
+
+pos_list =  open(pos_list_file, 'r').read().split('\n'):
+neg_list =  open(neg_list_file, 'r').read().split('\n'):
+
+
+classes_out = open(classes_files, 'w');
+for prot in pos_list:
+	acc = prot[0].split("|")[1]
+	classes_out.write('>' + acc + '\npositive\n')
+
+for prot in neg_list:
+	acc = prot[0].split("|")[1]
+	classes_out.write('>' + acc + '\nnegatvie\n')
+
+classes_out.close()
