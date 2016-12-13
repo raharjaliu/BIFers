1acd276f660b96f1681fd06ac25c967d22f4598d
Julian Schmidt <jschmidt@i12k-biolab08.informatik.tu-muenchen.de>
2016-12-13 16:22:30 +0100

    fixes

--- scripts/predict_profkernel.py
+++ scripts/predict_profkernel.py
@@ -11,15 +11,20 @@ infix = sys.argv[4]
 model_dir = sys.argv[5]
 out_dir = sys.argv[6]
 
+#python2.7 predict_profkernel.py /mnt/project/pp2_1617/xna_raharjaschmidt split 3 dna /mnt/project/pp2_1617/xna_raharjaschmidt/profkernel_models /mnt/project/pp2_1617/xna_raharjaschmidt/profkernel_predictions
+
 for i in range(0, number_splits):
-	fasta_file = os.path.join(training_data_dir, training_prefix + '.' + str(i) + '.' + training_infix + '.fasta')
-	profile_file = os.path.join(training_data_dir, training_prefix + '.' + str(i) + '.' + training_infix + '.profile')
+	fasta_file = os.path.join(data_dir, prefix + '.' + str(i) + '.' + infix + '.fasta')
+	print 'fasta_file: ' +  fasta_file
+	profile_file = os.path.join(data_dir, prefix + '.' + str(i) + '.' + infix + '.profile')
+	print 'profile_file: ' + profile_file
 
 	for j in range (0, number_splits):
 		for s in range(3, 9):
 			for k in range(2, s):
-				model_path = os.path.join(model_dir, prefix + '.i' + str(i) + '.j' + str(j) + . + infix + '.k' + str(k) + 's' + str(s))
-				if j != i and if os.path.isdir(model_path):
-					out_file = os.path.join(out_dir, prefix + '.' + str(j) + . + infix + '.k' + str(k) + 's' + str(s))
-					print ' '.join(['profkernel_workflow', '-f', fasta_file, '-p', profile_file, '-m', model_path, '-o', out_file])
-					subprocess.call(['profkernel_workflow', '-f', fasta_file, '-p', profile_file, '-m', model_path, '-o', out_file])
+				model_path = os.path.join(model_dir, prefix + '.' + str(j) + '.' + infix + '.k' + str(k) + 's' + str(s))
+				print 'model_path: ' + model_path
+				if j != i and os.path.isdir(model_path):
+					out_file = os.path.join(out_dir, prefix + '.i' + str(i) +  '.j' + str(j) + '.' + infix + '.k' + str(k) + 's' + str(s))
+					print ' '.join(['profkernel-workflow', '-f', fasta_file, '-p', profile_file, '-m', model_path, '-o', out_file])
+					subprocess.call(['profkernel-workflow', '-f', fasta_file, '-p', profile_file, '-m', model_path, '-o', out_file])
--- scripts/train_profkernel.py
+++ scripts/train_profkernel.py
@@ -4,7 +4,7 @@ import os.path
 import re
 import subprocess
 
-#python2.7 train_profkernel.py /mnt/project/pp2_1617/xna_raharjaschmidt split 3 rna /mnt/project/pp2_1617/xna_raharjaschmidt/profkernel_models
+#python2.7 train_profkernel.py /mnt/project/pp2_1617/xna_raharjaschmidt split 3 dna /mnt/project/pp2_1617/xna_raharjaschmidt/profkernel_models
 
 training_data_dir = sys.argv[1]
 training_prefix = sys.argv[2]
@@ -17,8 +17,10 @@ for i in range(0, number_splits):
 	profile_file = os.path.join(training_data_dir, training_prefix + '.' + str(i) + '.' + training_infix + '.profile')
 	class_file = os.path.join(training_data_dir, training_prefix + '.' + str(i) + '.' + training_infix + '.class.list')
 
-	for k in range(3, 8):
-		for s in range(2, k-1):
+	for s in range(3, 9):
+		for k in range(2, s):
 			out_dir = os.path.join(training_output_dir, training_prefix + '.' + str(i) + '.' + training_infix + '.k' + str(k) + 's' + str(s))
-			subprocess.call(['mkdir', out_dir])		
-			subprocess.call(['profkernel_workflow', '-f', fasta_file, '-p', profile_file, '-l', class_file, '-o', out_dir, '-k', k, '-s', s])
+			subprocess.call(['mkdir', out_dir])
+			args = ['profkernel-workflow', '-f', fasta_file, '-p', profile_file, '-l', class_file, '-o', out_dir, '-k', str(k), '-s', str(s)]	
+			print ' '.join(args)
+			subprocess.call(args)
