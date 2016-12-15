e0b583f3b85ff79d1200aabe680e8f3413fad1a9
Julian Schmidt <jschmidt@i12k-biolab09.informatik.tu-muenchen.de>
2016-12-15 09:44:34 +0100

    performance fixes

--- scripts/calculate_profkernel_performance.py
+++ scripts/calculate_profkernel_performance.py
@@ -8,24 +8,24 @@ import subprocess
 
 classes_dir = sys.argv[1]
 prediction_dir = sys.argv[2]
-prefix = sys.argv[2]
-number_splits = int(sys.argv[3])
-infix = sys.argv[4]
+prefix = sys.argv[3]
+number_splits = int(sys.argv[4])
+infix = sys.argv[5]
 out_file = sys.argv[6]
 
 out = open(out_file, 'w')
-out.write('\tAccurracy\tSensitivity\tSpecificty\tF1')
+out.write('\tAccurracy\tSensitivity\tSpecificty\tF1\n')
 
 for i in range(0, number_splits):
-	classes_file = os.path.join(classes_dir, prefix + '.' + i + '.' + infix + '.class.list')
+	classes_file = os.path.join(classes_dir, prefix + '.' + str(i) + '.' + infix + '.class.list')
 	classes_lines = open(classes_file).readlines()
 	classes = {}
 	curId = ''
 	for line in classes_lines:
 		if line.startswith('>'):
-			curId = line[1:]
+			curId = line[1:-1]
 		else:
-			classes[curId] = line
+			classes[curId] = line[:-1]
 	for j in range(0, number_splits):
 		for s in range(3, 9):
 			for k in range(2, s):
@@ -34,25 +34,26 @@ for i in range(0, number_splits):
 				tn = 0
 				fn = 0
 				prediction_file = os.path.join(prediction_dir, prefix + '.i' + str(i) +  '.j' + str(j) + '.' + infix + '.k' + str(k) + 's' + str(s))
-				prediction_lines = open(prediction_file, 'r').readlines()[2:]
-				prediction = {}
-				for line in prediction_lines:
-					split = line.split('\t')
-					prediction{split[0]} = split[1]
-					if split[1] == 'positive':
-						if classes[split[0]] == 'positive':
-							tp += 1
+				if os.path.isfile(prediction_file):
+					prediction_lines = open(prediction_file, 'r').readlines()[2:]
+					prediction = {}
+					for line in prediction_lines:
+						split = line.split('\t')
+						prediction[split[0]] = split[1]
+						if split[1] == 'positive':
+							if classes[split[0]] == 'positive':
+								tp += 1
+							else:
+								fp += 1
 						else:
-							fp += 1
-					else:
-						if classes[split[0]] == 'positive':
-							fn += 1
-						else:
-							tn += 1
-				if len(prediction_lines) > 0:
-					accurracy = (tp + tn)/(tp + fp + fn + tn)
-					sensitivity = tp/(tp + fn)
-					specificty = tn(fp + tn)
-					f1 = (2 * tp) / (2 * tp + fp + fn + tn)
-					out.write(prefix + '.i' + str(i) +  '.j' + str(j) + '.' + infix + '.k' + str(k) + 's' + str(s)) + '\t' + accurracy + '\t' + sensitivity + '\t' + specificty + '\t' + f1)
+							if classes[split[0]] == 'positive':
+								fn += 1
+							else:
+								tn += 1
+					if len(prediction_lines) > 0:
+						accurracy = (tp + tn) / float(tp + fp + fn + tn)
+						sensitivity = tp / float(tp + fn)
+						specificity = tn / float(fp + tn)
+						f1 = (2 * tp) / float(2 * tp + fp + fn)
+						out.write(prefix + '.i' + str(i) + '.j' + str(j) + '.' + infix + '.k' + str(k) + 's' + str(s) + '\t' + str(accurracy) + '\t' + str(sensitivity) + '\t' + str(specificity) + '\t' + str(f1) + '\n')
 out.close()
